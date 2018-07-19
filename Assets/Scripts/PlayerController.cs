b610fba8e4286a642ac6a6820b0ea0301cc81740
Tyukov Iliya <tyukovia@gmail.com>
2018-07-19 18:04:22 +0300

    Base character controls works. PixelPerfect camera setup (kinda)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,28 @@
 public class PlayerController : MonoBehaviour {
 
     Rigidbody2D rb;
+    [Header("Jump parameters:")]
     [SerializeField] float jumpForce = 5.25f;
-    private bool isGrounded;
-    public bool jumpRequest = false;
+    public bool isGrounded;
+    bool jumpRequest = false;
+    bool holdJumpButton = false;
     [SerializeField] int allowedJumps = 2;
-    int jumpsCount = 0;
+    public int jumpsCount;
 
+    [Header("Jump gravity variables:")]
+    [SerializeField] float fallMultiplier = 2.7f;
+    [SerializeField] float lowJumpMultiplier = 2f;
+
+    [Header("Ground parameters")]
     [SerializeField] Transform groundCheck;
-    const float groundedRadius = .2f;
+    const float groundedRadius = .1f;
     [SerializeField] LayerMask whatIsGround;
+
+    [Header("Spin parameters:")]
+    [SerializeField] float spinTime = 2f;
+
+    public bool spinAllow = true;
+    public bool spinRequest = false;
 
     void Awake ()
     {
@@ -25,34 +38,108 @@ public class PlayerController : MonoBehaviour {
             jumpRequest = true;
         }
         
+        
 	}
 
+
+    public void OnPressSpin()
+    {
+        spinRequest = true;
+        Spin();
+    }
+    public void OnReleaseSpin()
+    {
+        spinRequest = false;
+        Spin();
+    }
+
+
     private void FixedUpdate()
+    {
+        GroundCheck();
+        MultipleJumpProcessing();
+        GravityScaleChange();
+
+    }
+
+    public void Spin()
+    {
+        if (spinRequest && spinAllow)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+
+        }
+        else if (!spinRequest)
+        {
+            rb.constraints = RigidbodyConstraints2D.None | RigidbodyConstraints2D.FreezeRotation;
+            spinAllow = false;
+        }
+    }
+
+    public void JumpRequest()
+    {
+        jumpRequest = true;
+    }
+
+    private void GravityScaleChange()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallMultiplier;
+        }
+
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isGrounded)
+        {
+            transform.SetParent(collision.transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        transform.SetParent(null);
+    }
+
+
+    private void MultipleJumpProcessing()
+    {
+        if (jumpRequest && (jumpsCount < allowedJumps))
+        {
+            jumpsCount++;
+            Jump();
+            jumpRequest = false;
+
+        }
+        else
+        {
+            jumpRequest = false;
+        }
+    }
+
+    private void GroundCheck()
     {
         bool wasGrounded = isGrounded;
         isGrounded = false;
 
-        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
             {
                 isGrounded = true;
-
-            }
-
-
-            if (jumpRequest && (jumpsCount < allowedJumps))
-            {
-                Jump();
-                jumpRequest = false;
-                jumpsCount++;
+                jumpsCount = 1;
+                spinAllow = true;
             }
         }
     }
-
     private void Jump()
     {
         Vector2 velocity = rb.velocity;
---
+++ Assets/death.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
