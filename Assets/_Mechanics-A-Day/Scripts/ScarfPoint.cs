e8a7b2837b812182507813bcf23ae92ca72fda3e
Tyukov Ilya <tyukovia@gmail.com>
2019-02-07 18:12:35 +0300

    Scarf work continues

--- Assets/_Mechanics-A-Day/Scripts/ScarfPoint.cs
+++ Assets/_Mechanics-A-Day/Scripts/ScarfPoint.cs
@@ -9,14 +9,42 @@ public class ScarfPoint : MonoBehaviour
     private Vector3 velocity;
     public float smthDampTime = .2f;
     bool playerFacingLeft = false;
+    Vector2 playerVelocity;
+    public Vector3 offsetVector;
 
     void Update()
     {
-        playerFacingLeft = GameController.Instance.player.isFacingLeft;
-        if (playerFacingLeft)
-            offset.x = Mathf.Abs(offset.x);
+        playerVelocity = GameController.Instance.player.velocity;
+        if (playerVelocity.x == 0)
+        {
+            playerFacingLeft = GameController.Instance.player.isFacingLeft;
+            if (playerFacingLeft)
+                offsetVector.x = Mathf.Abs(offset.x);
+            else
+                offsetVector.x = Mathf.Abs(offset.x) * -1;
+        }
         else
-            offset.x = Mathf.Abs(offset.x) * -1;
-        transform.position = Vector3.SmoothDamp(transform.position, targetToFollow.position + offset, ref velocity, smthDampTime);
+        {
+            offsetVector.x = Mathf.Sign(playerVelocity.x) * Mathf.Abs(offset.x)*-1;
+        }
+        if (playerVelocity.y <= .002f && playerVelocity.y >= -.002f)
+            offsetVector.y = Mathf.Abs(offset.y) * -1;
+        else
+            offsetVector.y = 0;
+
+
+        //offsetVector = new Vector3(
+        //    Mathf.Sign(playerVelocity.x)*offset.x,
+        //Mathf.Sign(-playerVelocity.y)*offset.y,
+        //0
+        //);
+        //dotPosition = new Vector3(
+        //    targetToFollow.position.x + dotPosition.x * -Mathf.Clamp(playerVelocity.x, -1, 1),
+        //    targetToFollow.position.y + dotPosition.y * -Mathf.Clamp(playerVelocity.y, -1, 1),
+        //    0
+        //    );
+
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetToFollow.position + offsetVector, ref velocity, smthDampTime);
     }
 }
